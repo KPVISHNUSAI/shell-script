94d8b37357b6bb4a9cd73393c02a8cf7bd4f950c
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2025-09-16 19:58:29 +0530

    added conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Rahim: Hi Robert"
-echo "Robert: Hello Rahim"
-echo "Rahim: How are you doing?"
-echo "Robert: I'm doing great, thanks for asking!"
+PERSON1="Vishnu"
+PERSON2="Adithya"
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you doing?"
+echo "$PERSON2: I'm doing great, thanks for asking!"
